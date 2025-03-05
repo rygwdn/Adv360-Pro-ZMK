e259d1edaba4a1d12cb62e8db3b889e85776c491
Ryan Wooden <rygwdn@gmail.com>
2025-03-04 21:23:16 -0400

    improve fetch script

--- bin/fetch-build.sh
+++ bin/fetch-build.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-rm firmware/*
+build_name="Build ZMK firmware"
+
+rm -f firmware/*
+
 sha=$(git rev-parse --verify HEAD)
-run=$(gh run list -c "$sha" --json databaseId -q '.[0].databaseId')
-gh run download -n firmware -D firmware/ "$run"
+run=$(gh run list -c "$sha" --json databaseId,workflowName -q ".[] | select(.workflowName == \"$build_name\") | .databaseId")
+
+if [[ -z $run ]]; then
+  echo "No \"${build_name}\" run for $sha"
+  exit 1
+fi
+
+if ! gh run download -n firmware -D firmware/ "$run"; then
+  exit 1
+fi
+
+echo "Fetched build:"
+GH_PAGER='' gh run view "$run"
